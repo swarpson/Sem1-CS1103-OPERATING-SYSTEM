8af8cbac7b304ad0fbedaa0b3a6ee62ccd8e3ca5
Swaroop R <swarooprbca24@rvu.edu.in>
2025-04-07 14:09:17 +0530

    Add files via upload

---
+++ Shutdown_with_wall_broadcast_message.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Broadcast the message to all logged-in users
+wall "System is scheduled to shut down at 3:45 PM. Please save your work and log out."
+
+# Schedule the system to shut down at 3:45 PM
+shutdown 15:45
